949c2c6e9f125e8503c885820353d43d9b99b05f
Chris Abdo <christophermabdo@icloud.com>
2021-04-08 17:02:39 +0100

    added test for airport holding planes

---
+++ apps/airport_test.py
@@ -0,0 +1,12 @@
+import pytest
+from airport import Airport
+NUMBER_1 = 3
+NUMBER_2 = 2
+
+def test_add():
+    value = NUMBER_1 + NUMBER_2
+    assert value == 5
+
+def test_airport_has_hangar():
+    airport = Airport()
+    assert airport.hangar == []
--- tests/airport_test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-NUMBER_1 = 3
-NUMBER_2 = 2
-
-def test_add():
-    value = NUMBER_1 + NUMBER_2
-    assert value == 5
