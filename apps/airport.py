440c907e965df34c4bc222ee17c8d97dbe9552e6
Chris Abdo <christophermabdo@icloud.com>
2021-04-12 16:04:44 +0100

    capacity maximum set and planes cannot land if reached. test green

--- apps/airport.py
+++ apps/airport.py
@@ -1,18 +1,17 @@
 class Airport:
 
-    MAXIMUM_1 = 3
+    
     def __init__(self):
+        self.MAXIMUM_CAPACITY = 3
         self.hangar = []
 
     def land(self, plane):
-        self.full_capacity(plane)
+        if len(self.hangar) == self.MAXIMUM_CAPACITY:
+          return "Hangar Capacity Reached!"
+        else:
+          self.hangar.append(plane)
 
     def take_off(self, plane):
         self.hangar.remove(plane)
 
-    def full_capacity(self, plane):
-        if len(self.hangar) == 3:
-            return "Hangar Capacity Reached!"
-        else:
-            self.hangar.append(plane)
-
+    # def full_capacity(self, plane)
